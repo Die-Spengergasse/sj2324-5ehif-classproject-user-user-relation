a0968e47bb7f526bb572fa8db366deed44ab3beb
Alma Löblich <LOE20420@spengergasse.at>
2023-11-06 14:57:38 +0100

    creation of dto classes for user

---
+++ cooking-user/sj2324-5ehif-cooking-user.Application/DTO/DtoMappingProfile.cs
@@ -0,0 +1,43 @@
+﻿using AutoMapper;
+using sj2324_5ehif_cooking_user.Application.DTO;
+using sj2324_5ehif_cooking_user.Application.Model;
+class DtoMappingProfile : Profile
+{
+    public DtoMappingProfile()
+    {
+        // Mapping for User and UserDto
+        CreateMap<User, UserDto>();
+        CreateMap<UserDto, User>()
+            .BeforeMap((src, dst) =>
+            {
+                if (string.IsNullOrEmpty(src.Username)) { throw new ApplicationException("Invalid username."); }
+                if (string.IsNullOrEmpty(src.Lastname)) { throw new ApplicationException("Invalid lastname."); }
+                if (string.IsNullOrEmpty(src.Firstname)) { throw new ApplicationException("Invalid firstname."); }
+                if (string.IsNullOrEmpty(src.Email)) { throw new ApplicationException("Invalid email."); }
+            });
+
+        // Mapping for Cookbook and CookbookDto
+        CreateMap<Cookbook, CookbookDto>();
+        CreateMap<CookbookDto, Cookbook>()
+            .BeforeMap((src, dst) =>
+            {
+                if (string.IsNullOrEmpty(src.Name)) { throw new ApplicationException("Invalid cookbook name."); }
+            });
+
+        // Mapping for Recipe and RecipeDto
+        CreateMap<Recipe, RecipeDto>();
+        CreateMap<RecipeDto, Recipe>()
+            .BeforeMap((src, dst) =>
+            {
+                if (string.IsNullOrEmpty(src.Name)) { throw new ApplicationException("Invalid recipe name."); }
+            });
+
+        // Mapping for Preference and PreferenceDto
+        CreateMap<Preference, PreferenceDto>();
+        CreateMap<PreferenceDto, Preference>()
+            .BeforeMap((src, dst) =>
+            {
+                if (string.IsNullOrEmpty(src.Name)) { throw new ApplicationException("Invalid preference name."); }
+            });
+    }
+}
