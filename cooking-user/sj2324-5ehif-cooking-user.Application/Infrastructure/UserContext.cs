f08b71a75acf8777f31c0e754e5a047674812ae7
DenesTak <denes.takacs@protonmail.com>
2023-11-14 14:13:38 +0100

    fix(api): fixed model/dbContext

---
+++ cooking-user/sj2324-5ehif-cooking-user.Application/Infrastructure/UserContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using sj2324_5ehif_cooking_user.Application.Model;
+
+namespace sj2324_5ehif_cooking_user.Application.Infrastructure
+{
+    public class UserContext : DbContext
+    {
+        public DbSet<Cookbook> Cookbooks => Set<Cookbook>();
+        public DbSet<Preference> Preferences => Set<Preference>();
+        public DbSet<Recipe> Recipes => Set<Recipe>();
+        public DbSet<User> Users => Set<User>();
+        public UserContext(DbContextOptions<UserContext> options) : base(options)
+        {
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+        }
+    }
+}
---
+++ cooking-user/sj2324-5ehif-cooking-user.Webapi/Startup.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using sj2324_5ehif_cooking_user.Application.Infrastructure;
+
+namespace sj2324_5ehif_cooking_user.Webapi;
+
+public class Startup
+{
+    private IConfigurationRoot Configuration { get; }
+    public Startup(IConfigurationRoot configuration)
+    {
+        Configuration = configuration;
+    }
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddDbContext<UserContext>(options =>
+            options.UseNpgsql(Configuration.GetConnectionString("PostgresConnection")));
+    }
+    public async Task Configure(WebApplication app)
+    {
+        using (var scope = app.Services.CreateScope())
+        {
+            var services = scope.ServiceProvider;
+            await services.GetRequiredService<UserContext>().Database.EnsureCreatedAsync();
+        }
+        
+        if (app.Environment.IsDevelopment())
+        {
+            //    app.UseSwagger();
+            //    app.UseSwaggerUI();
+        }
+
+        app.UseHttpsRedirection();
+        app.UseAuthorization();
+        app.MapControllers();
+    }
+}
