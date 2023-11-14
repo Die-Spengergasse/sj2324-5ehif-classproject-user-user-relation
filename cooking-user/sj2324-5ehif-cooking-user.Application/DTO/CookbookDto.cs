97c5c01b091bb0882352c5588a19f8f5e439facf
Alma Löblich <LOE20420@spengergasse.at>
2023-11-14 09:54:23 +0100

    add user dtos

---
+++ cooking-user/sj2324-5ehif-cooking-user.Application/DTO/CookbookDto.cs
@@ -0,0 +1,23 @@
+﻿using sj2324_5ehif_cooking_user.Application.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory;
+
+namespace sj2324_5ehif_cooking_user.Application.DTO
+{
+    public class CookbookDto
+    {
+        public string? CookbookKey { get; set; }
+        public UserDto? Owner { get; set; }
+        public string? Name { get; set; }
+        public bool? Private { get; set; }
+        public List<RecipeDto>? Recipes { get; set; } = new();
+        public List<UserDto>? Collaborators { get; set; } = new();
+    }
+}
+
+
+
---
+++ cooking-user/sj2324-5ehif-cooking-user.Application/DTO/UserDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sj2324_5ehif_cooking_user.Application.DTO
+{
+    public class UserDto
+    {
+        public string? UserKey { get; set; }
+        public string? Username { get; set; }
+        public string? Lastname { get; set; }
+        public string? Firstname { get; set; }
+        public string? Email { get; set; }
+        public List<PreferenceDto>? Preferences { get; set; } = new();
+    }
+}
