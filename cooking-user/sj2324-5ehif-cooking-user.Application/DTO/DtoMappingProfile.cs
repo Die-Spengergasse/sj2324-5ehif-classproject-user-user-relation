db31733ecd5e8de0bb742a169cc4ca9e2d7679bf
XenoxJeager <60194221+XenoxJeager@users.noreply.github.com>
2023-11-15 16:17:52 +0100

    Fix DTO+Model Classes

--- cooking-user/sj2324-5ehif-cooking-user.Application/DTO/DtoMappingProfile.cs
+++ cooking-user/sj2324-5ehif-cooking-user.Application/DTO/DtoMappingProfile.cs
@@ -1,5 +1,6 @@
 ﻿using AutoMapper;
 using sj2324_5ehif_cooking_user.Application.Model;
+using ApplicationException = System.ApplicationException;
 
 namespace sj2324_5ehif_cooking_user.Application.DTO
 {
@@ -10,23 +11,48 @@ namespace sj2324_5ehif_cooking_user.Application.DTO
             CreateMap<UserDto, User>()
                 .BeforeMap((src, dst) =>
                 {
-                    if (string.IsNullOrEmpty(src.Username)) { throw new ApplicationException("Invalid username"); }
-                    if (string.IsNullOrEmpty(src.Lastname)) { throw new ApplicationException("Invalid lastname"); }
-                    if (string.IsNullOrEmpty(src.Firstname)) { throw new ApplicationException("Invalid firstname"); }
-                    if (string.IsNullOrEmpty(src.Email)) { throw new ApplicationException("Invalid email"); }
+                    // change to get values from DB
+                    var list = new List<Preference>();
+                    foreach (var preference in src.Preferences)
+                    {
+                        dst.AddPreference(new Preference(preference.Name){Key = preference.PreferenceKey});
+                        
+                    }
+                    
+                    dst.Key = src.UserKey;
+                    if (string.IsNullOrEmpty(src.Username))
+                    {
+                        throw new ApplicationException("Invalid username");
+                    }
+
+                    if (string.IsNullOrEmpty(src.Lastname))
+                    {
+                        throw new ApplicationException("Invalid lastname");
+                    }
+
+                    if (string.IsNullOrEmpty(src.Firstname))
+                    {
+                        throw new ApplicationException("Invalid firstname");
+                    }
+
+                    if (string.IsNullOrEmpty(src.Email))
+                    {
+                        throw new ApplicationException("Invalid email");
+                    }
                 });
-            
+
             CreateMap<PreferenceDto, Preference>()
-                .ConstructUsing(dto => new Preference(dto.Name));
-            
+                .ConstructUsing(dto => new Preference(dto.Name) { Key = dto.PreferenceKey });
+
             CreateMap<User, UserDto>()
-             .BeforeMap((src, dst) => dst.UserKey = src.Key.Value);
+                .BeforeMap((src, dst) => dst.UserKey = src.ObjectKey.Value)
+                .ForSourceMember(source => source.Preferences, opt => opt.DoNotValidate());
 
             CreateMap<Preference, PreferenceDto>()
                 .BeforeMap((src, dst) => dst.PreferenceKey = src.ProxyKey.Value);
-            
+
             CreateMap<Cookbook, CookbookDto>()
-              .ForMember(dest => dest.CookbookKey, opt => opt.MapFrom(src => src.Key.Value));
+                .ForMember(dest => dest.CookbookKey, opt => opt.MapFrom(src => src.KeyObject.Value));
 
             CreateMap<Recipe, RecipeDto>()
                 .ForMember(dest => dest.RecipeKey, opt => opt.MapFrom(src => src.ProxyId.Value));
@@ -34,21 +60,31 @@ namespace sj2324_5ehif_cooking_user.Application.DTO
             CreateMap<CookbookDto, Cookbook>()
                 .BeforeMap((src, dst) =>
                 {
-                    if (string.IsNullOrEmpty(src.Name)) { throw new ApplicationException("Invalid cookbook name."); }
+                    dst.Key = src.CookbookKey;
+                    foreach (var collaborator in src.Collaborators)
+                    {
+                        dst.AddUser(new User(username:collaborator.Username,lastname:collaborator.Firstname,firstname:collaborator.Lastname,email:collaborator.Email){Key = collaborator.UserKey});
+                    }
+
+                    foreach (var recipe in src.Recipes)
+                    {
+                        dst.AddRecipe(new Recipe(name:recipe.Name){Id=recipe.RecipeKey});
+                    }
+                    if (string.IsNullOrEmpty(src.Name))
+                    {
+                        throw new ApplicationException("Invalid cookbook name.");
+                    }
                 });
 
-            
-                     
-            CreateMap<PreferenceDto, Preference>()
-                .ConstructUsing(dto => new Preference(dto.Name));
 
             CreateMap<RecipeDto, Recipe>()
-            .ConstructUsing(dto => new Recipe(dto.Name)).
-            BeforeMap((src, dst) =>
-            {
-                if (string.IsNullOrEmpty(src.Name)) { throw new ApplicationException("Invalid recipe name."); }
-            });
-
+                .ConstructUsing(dto => new Recipe(dto.Name){Id = dto.RecipeKey}).BeforeMap((src, dst) =>
+                {
+                    if (string.IsNullOrEmpty(src.Name))
+                    {
+                        throw new ApplicationException("Invalid recipe name.");
+                    }
+                });
         }
     }
 }
--- cooking-user/sj2324-5ehif-cooking-user.Application/Model/UserKey.cs
+++ cooking-user/sj2324-5ehif-cooking-user.Application/Model/UserKey.cs
@@ -7,4 +7,7 @@ public class UserKey : Key
     public UserKey() : base(prefix: "USR", length: 14)
     {
     }
+    public UserKey(string value) : base(value:value,prefix:"USR", length:14)
+    {
+    }
 } 
