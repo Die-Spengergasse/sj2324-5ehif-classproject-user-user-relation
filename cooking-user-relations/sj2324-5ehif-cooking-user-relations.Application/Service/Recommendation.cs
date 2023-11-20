fa1f0b4ab82f0509d1e3f79b8160d6d6a63b098c
Jeffhuh <Jeff.hu@gmx.at>
2023-11-20 11:08:34 +0100

    Bases for recommendation algorithm

---
+++ cooking-user-relations/sj2324-5ehif-cooking-user-relations.Application/Service/Recommendation.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+public class Recommendation
+{
+	public Recommendation()
+	{
+
+	}
+
+	public int Recommend()
+	{
+		return new Random().Next();
+	} // does the teacher even read the commits?
+}
