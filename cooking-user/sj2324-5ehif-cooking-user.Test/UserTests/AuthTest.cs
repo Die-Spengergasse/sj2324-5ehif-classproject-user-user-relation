343526af1f347b2e735f789bdf35d665ec0eb3ce
Tristan LOSADA BENINI <los20421@spengergasse.at>
2023-11-26 00:20:56 +0100

    feat(test): add tests for Auth & User Controllers
    very little test coverage, will be implemented

---
+++ cooking-user/sj2324-5ehif-cooking-user.Test/UserTests/AuthTest.cs
@@ -0,0 +1,86 @@
+using Bogus;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using sj2324_5ehif_cooking_user.Application.Infrastructure;
+using sj2324_5ehif_cooking_user.Application.Model;
+using sj2324_5ehif_cooking_user.Webapi.Controllers;
+using sj2324_5ehif_cooking_user.Webapi.Services;
+
+namespace sj2324_5ehif_cooking_user.Test.UserTests;
+
+public class AuthTest
+{
+    private readonly IConfiguration _mockConfiguration;
+    private readonly UserContext _mockContext;
+    private readonly JwtUtils _mockJwtUtils;
+    private readonly ILogger<AuthController> _mockLogger;
+    private readonly IPasswordUtils _mockPasswordUtils;
+
+    public AuthTest()
+    {
+        var options = new DbContextOptionsBuilder<UserContext>()
+            .UseInMemoryDatabase("TestDatabase")
+            .Options;
+
+        _mockContext = new UserContext(options);
+        var faker = new Faker();
+
+        _mockLogger = new Logger<AuthController>(new LoggerFactory());
+        _mockJwtUtils = new JwtUtils(_mockConfiguration);
+        _mockPasswordUtils = new PasswordUtils();
+
+        _mockContext.Users.AddRange(new User
+        (
+            faker.Internet.UserName(),
+            firstname: faker.Name.FirstName(),
+            lastname: faker.Name.LastName(),
+            email: faker.Internet.Email(),
+            password: "hashedpassword"
+        ));
+
+        _mockContext.SaveChanges();
+    }
+
+    [Fact]
+    public async Task Register_ValidUser_ReturnsOk()
+    {
+        var controller = new AuthController(_mockContext, _mockLogger,
+            _mockJwtUtils, _mockPasswordUtils);
+
+        var faker = new Faker();
+        var registerModel = new RegisterModel
+        {
+            Username = faker.Internet.UserName(),
+            Lastname = faker.Name.LastName(),
+            Firstname = faker.Name.FirstName(),
+            Email = faker.Internet.Email(),
+            Password = "hashedpassword"
+        };
+
+        var result = await controller.Register(registerModel) as OkObjectResult;
+
+        Assert.NotNull(result);
+        Assert.Equal("Registration successful", result.Value);
+    }
+
+    [Fact]
+    public async Task Login_ValidCredentials_ReturnsToken()
+    {
+        var controller = new AuthController(_mockContext, _mockLogger,
+            _mockJwtUtils, _mockPasswordUtils);
+
+        var loginModel = new LoginModel
+        {
+            Username = _mockContext.Users.First().Username,
+            Password = "hashedpassword"
+        };
+
+        var result = await controller.Login(loginModel) as OkObjectResult;
+
+        Assert.NotNull(result);
+        dynamic tokenResult = result.Value;
+        Assert.NotNull(tokenResult.token);
+    }
+}
---
+++ cooking-user/sj2324-5ehif-cooking-user.Test/UserTests/UserTest.cs
@@ -0,0 +1,65 @@
+using AutoMapper;
+using Bogus;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using sj2324_5ehif_cooking_user.Application.DTO;
+using sj2324_5ehif_cooking_user.Application.Infrastructure;
+using sj2324_5ehif_cooking_user.Application.Model;
+using sj2324_5ehif_cooking_user.Webapi.Controllers;
+using sj2324_5ehif_cooking_user.Webapi.Services;
+
+namespace sj2324_5ehif_cooking_user.Test.UserTests;
+
+public class UserTest
+{
+    private readonly UserContext _mockContext;
+    private readonly ILogger<UserController> _mockLogger;
+    private readonly IMapper _mockMapper;
+    private readonly IPasswordUtils _mockPasswordUtils;
+
+    public UserTest()
+    {
+        var options = new DbContextOptionsBuilder<UserContext>()
+            .UseInMemoryDatabase(databaseName: "TestDatabase")
+            .Options;
+
+        _mockContext = new UserContext(options);
+        var faker = new Faker();
+
+        _mockLogger = new Logger<UserController>(new LoggerFactory());
+        _mockPasswordUtils = new PasswordUtils();
+        _mockMapper = new Mapper(new MapperConfiguration(cfg => cfg.CreateMap<PreferenceDto, Preference>()));
+
+        _mockContext.Users.AddRange(new User
+        (
+            username: faker.Internet.UserName(),
+            firstname: faker.Name.FirstName(),
+            lastname: faker.Name.LastName(),
+            email: faker.Internet.Email(),
+            password: "hashedpassword"
+        ));
+
+        _mockContext.SaveChanges();
+    }
+    
+    [Fact]
+    public async Task DeleteUser_InvalidCredentials_ReturnsUnauthorized()
+    {
+        var controller = new UserController(_mockContext, _mockLogger, _mockPasswordUtils, _mockMapper);
+
+        var userToDelete = _mockContext.Users.First();
+
+        var deleteModel = new DeleteUserModel
+        {
+            Username = userToDelete.Username,
+            Email = userToDelete.Email,
+            Password = "invalidpassword"
+        };
+
+        var result = await controller.DeleteUser(deleteModel) as UnauthorizedObjectResult;
+
+        Assert.NotNull(result);
+        Assert.Equal("Invalid credentials", result.Value);
+    }
+}
