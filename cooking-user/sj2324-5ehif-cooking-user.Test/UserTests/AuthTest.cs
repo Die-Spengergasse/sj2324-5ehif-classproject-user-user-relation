ddbc30c459848f404aa6ddad7510e07d9c128c96
Tristan LOSADA BENINI <los20421@spengergasse.at>
2023-11-27 09:38:49 +0100

    feat(test): add auth & user controller tests

--- cooking-user/sj2324-5ehif-cooking-user.Test/UserTests/AuthTest.cs
+++ cooking-user/sj2324-5ehif-cooking-user.Test/UserTests/AuthTest.cs
@@ -5,7 +5,6 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using sj2324_5ehif_cooking_user.Application.DTO;
 using sj2324_5ehif_cooking_user.Application.Infrastructure;
-using sj2324_5ehif_cooking_user.Application.Model;
 using sj2324_5ehif_cooking_user.Webapi.Controllers;
 using sj2324_5ehif_cooking_user.Webapi.Services;
 
@@ -21,36 +20,36 @@ public class AuthTest
 
     public AuthTest()
     {
+        var inMemorySettings = new Dictionary<string, string>
+        {
+            {
+                "Jwt:Key",
+                "ecawiasqrpqrgyhwnolrudpbsrwaynbqdayndnmcehjnwqyouikpodzaqxivwkconwqbhrmxfgccbxbyljguwlxhdlcvxlutbnwjlgpfhjgqbegtbxbvwnacyqnltrby"
+            },
+            { "Jwt:Issuer", "CookingUser" },
+            { "Jwt:Audience", "CookingUser" }
+        };
+
+
+        _mockConfiguration = new ConfigurationBuilder()
+            .AddInMemoryCollection(inMemorySettings)
+            .Build();
+
         var options = new DbContextOptionsBuilder<UserContext>()
             .UseInMemoryDatabase("TestDatabase")
             .UseLoggerFactory(LoggerFactory.Create(builder => builder.AddConsole()))
             .Options;
 
         _mockContext = new UserContext(options);
-        var faker = new Faker();
 
         _mockLogger = new Logger<AuthController>(new LoggerFactory());
         _mockJwtUtils = new JwtUtils(_mockConfiguration);
         _mockPasswordUtils = new PasswordUtils();
-
-        _mockContext.Users.Add(new User
-        (
-            "testuser",
-            firstname: faker.Name.FirstName(),
-            lastname: faker.Name.LastName(),
-            email: faker.Internet.Email(),
-            password: "hashedpassword"
-        ));
-
-        _mockContext.SaveChanges();
     }
 
     [Fact]
     public async Task Register_ValidUser_ReturnsOk()
     {
-        var controller = new AuthController(_mockContext, _mockLogger,
-            _mockJwtUtils, _mockPasswordUtils);
-
         var faker = new Faker();
         var registerModel = new RegisterModel
         {
@@ -58,12 +57,56 @@ public class AuthTest
             Lastname = faker.Name.LastName(),
             Firstname = faker.Name.FirstName(),
             Email = faker.Internet.Email(),
-            Password = "hashedpassword"
+            Password = faker.Internet.Password()
         };
 
-        var result = await controller.Register(registerModel) as OkObjectResult;
+        var result = await RegistrationHelper(registerModel) as OkObjectResult;
 
         Assert.NotNull(result);
         Assert.Equal("Registration successful", result.Value);
+    }
+
+    [Fact]
+    public async Task Login_ValidUser_ReturnsToken()
+    {
+        var faker = new Faker();
+        var registerModel = new RegisterModel
+        {
+            Username = "testuser",
+            Lastname = faker.Name.LastName(),
+            Firstname = faker.Name.FirstName(),
+            Email = faker.Internet.Email(),
+            Password = "hashedpassword"
+        };
+
+        await RegistrationHelper(registerModel);
+
+
+        var loginModel = new LoginModel
+        {
+            Username = "testuser",
+            Password = "hashedpassword"
+        };
+
+        var result = await LoginHelper(loginModel) as OkObjectResult;
+
+        Assert.NotNull(result.Value);
+    }
+
+    //Helper methods
+    private async Task<IActionResult> RegistrationHelper(RegisterModel registerModel)
+    {
+        var controller = new AuthController(_mockContext, _mockLogger,
+            _mockJwtUtils, _mockPasswordUtils);
+
+        return await controller.Register(registerModel);
+    }
+
+    private async Task<IActionResult> LoginHelper(LoginModel loginModel)
+    {
+        var controller = new AuthController(_mockContext, _mockLogger,
+            _mockJwtUtils, _mockPasswordUtils);
+
+        return await controller.Login(loginModel);
     }
 }
