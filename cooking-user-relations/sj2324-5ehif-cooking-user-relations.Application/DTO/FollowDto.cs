a5ef27cdc182027b624f594b4f961331756a0997
AlmaLoeb <loe20420@spengergasse.at>
2023-11-27 18:38:54 +0100

    first draft of dtos and tests

---
+++ cooking-user-relations/sj2324-5ehif-cooking-user-relations.Application/DTO/FollowDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sj2324_5ehif_cooking_user_relations.Application.DTO
+{
+    public class FollowDto
+    {
+        public string FollowKey { get; set; }
+        public UserDto Follower { get; set; }
+        public UserDto Followed { get; set; }
+    }
+}
---
+++ cooking-user-relations/sj2324-5ehif-cooking-user-relations.Application/DTO/RecipeShareDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sj2324_5ehif_cooking_user_relations.Application.DTO
+{
+    public class RecipeShareDto
+    {
+        public string RecipeShareKey { get; set; } 
+        public RecipeDto Recipe { get; set; } 
+        public List<UserDto> Collaborators { get; set; } = new List<UserDto>();
+
+    }
+}
