bf4de1638e3765ec5cfd08faa818dcba1d06e827
AlmaLoeb <loe20420@spengergasse.at>
2023-11-27 09:03:49 +0100

    user relations context + test

---
+++ cooking-user-relations/sj2324-5ehif-cooking-user-relations.Test/DatabaseTest.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using sj2324_5ehif_cooking_user_relations.Application.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sj2324_5ehif_cooking_user_relations.Test
+{
+    public class DatabaseTest : IDisposable
+    {
+
+        protected readonly SqliteConnection _connection;
+
+        protected readonly DbContextOptions<UserRelationsContext> _options;
+
+        protected readonly UserRelationsContext _context;
+
+
+        public DatabaseTest()
+        {
+
+            _connection = new SqliteConnection("DataSource=:memory:");
+
+            _connection.Open();
+
+            _options = new DbContextOptionsBuilder<UserRelationsContext>().UseSqlite(_connection).Options;
+
+            _context = new UserRelationsContext(_options);
+
+            _context.Database.EnsureDeleted();
+            _context.Database.EnsureCreated();
+
+        }
+
+
+        public void Dispose()
+        {
+            _connection.Dispose();
+        }
+    }
+}
---
+++ cooking-user-relations/sj2324-5ehif-cooking-user-relations.Test/UserRelationsContextTest.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using sj2324_5ehif_cooking_user_relations.Application.Infrastructure;
+using sj2324_5ehif_cooking_user_relations.Application.Model;
+using Xunit;
+
+namespace sj2324_5ehif_cooking_user_relations.Test
+{
+    public class UserRelationsContextTest
+    {
+        
+            [Fact]
+            public void CreateDatabaseTest()
+            {
+                Assert.True(true);  
+            }
+        }
+    }
