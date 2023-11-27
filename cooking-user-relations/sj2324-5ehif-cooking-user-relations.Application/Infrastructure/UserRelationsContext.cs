c427832b81b5e9064c94e760a3862343e09bce4c
AlmaLoeb <loe20420@spengergasse.at>
2023-11-27 09:03:49 +0100

    user relations context + test

---
+++ cooking-user-relations/sj2324-5ehif-cooking-user-relations.Application/Infrastructure/UserRelationsContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using sj2324_5ehif_cooking_user_relations.Application.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sj2324_5ehif_cooking_user_relations.Application.Infrastructure
+{
+
+        public class UserRelationsContext : DbContext
+        {
+            public DbSet<Feedback> Feedbacks => Set<Feedback>();
+            public DbSet<Follow> Follows => Set<Follow>();
+            public DbSet<RecipeShare> RecipeShares => Set<RecipeShare>();
+            public DbSet<Recipe> Recipes => Set<Recipe>();
+            public DbSet<User> Users => Set<User>();
+            public UserRelationsContext(DbContextOptions<UserRelationsContext> options) : base(options)
+            {
+            }
+            protected override void OnModelCreating(ModelBuilder modelBuilder)
+            {
+            }
+        }
+    
+
+}
