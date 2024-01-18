412d2d073adc2b4f81d73c6189e5a626a54f198d
AlmaLoeb <80312433+AlmaLoeb@users.noreply.github.com>
2024-01-18 08:09:33 +0100

    Fix/cors frontendtobackend (#35)
    * cors development added fronted communication to api endpoint backend
    
    * fixed ports again

--- cooking-user/sj2324-5ehif-cooking-user.Webapi/Program.cs
+++ cooking-user/sj2324-5ehif-cooking-user.Webapi/Program.cs
@@ -1,6 +1,12 @@
 using sj2324_5ehif_cooking_user.Webapi;
 
 var builder = WebApplication.CreateBuilder(args);
+
+if (builder.Environment.IsDevelopment())
+{
+    builder.Services.AddCors(options =>
+        options.AddDefaultPolicy(builder => builder.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod()));
+}
 var startup = new Startup(builder.Configuration);
 
 startup.ConfigureServices(builder.Services);
