eaaca0e2d5c83c22515ff1913a1d9db135c8404f
DenesTak <denes.takacs@protonmail.com>
2024-01-20 22:16:55 +0100

    feat(user): add automated preference importer

---
+++ cooking-user/sj2324-5ehif-cooking-user.Webapi/Services/PreferenceService/FillPreferenceTable.cs
@@ -0,0 +1,40 @@
+﻿using System.Globalization;
+using CsvHelper;
+using CsvHelper.Configuration;
+using sj2324_5ehif_cooking_user.Application.Infrastructure;
+using sj2324_5ehif_cooking_user.Application.Model;
+
+namespace sj2324_5ehif_cooking_user.Webapi.Services;
+
+// dont change to abstract !!!
+public class PreferenceRecord
+{
+    public string Name { get; set; }
+}
+
+public class FillPreferenceTable
+{
+    private readonly UserContext _context;
+
+    public FillPreferenceTable(UserContext context)
+    {
+        _context = context;
+    }
+
+    public void ReadCSV()
+    {
+        var config = new CsvConfiguration(CultureInfo.InvariantCulture) { HasHeaderRecord = false };
+        var reader = new StreamReader("./Services/PreferenceService/preference.csv");
+        var csv = new CsvReader(reader, config);
+        var records = csv.GetRecords<PreferenceRecord>();
+
+        foreach (var record in records)
+        {
+            if (_context.Preferences.Any(p => p.Name == record.Name)) continue;
+            var preference = new Preference(record.Name);
+            _context.Preferences.Add(preference);
+        }
+
+        _context.SaveChanges();
+    }
+}
